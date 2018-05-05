0799e06cdb601da76a4b3185f3a223d5d8dcc75e
rexcacayo <rflugaresir@gmail.com>
2018-05-05 13:22:56 +0100

    first commit

---
+++ fullwidth.html
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Tema de Ejemplo</title>
+	
+	<link rel="stylesheet" href="style.css" >
+
+</head>
+
+<body>
+	
+	<div id="global-container">
+		
+		
+		<header id="main-header">
+			
+			<h1 class="site-title">El nombre del sitio</h1>
+			<h2 class="site-description">En este lugar debe ir la descripción del sitio</h2>
+		
+		</header><!-- /#main-header -->
+		
+		
+		<nav id="main-nav">
+			<ul class="menu">
+				<li><a href="">Menú item 1</a></li>
+				<li><a href="">Menú item 2</a></li>
+				<li><a href="">Menú item 3</a></li>
+				<li><a href="">Menú item 4</a></li>
+				<li><a href="">Menú item 5</a></li>
+				<li><a href="">Menú item 6</a></li>
+			</ul>
+		</nav><!-- /#main-nav -->
+		
+		
+		<section id="global-content" class="cf">
+			
+			<section id="main-content" class="fullwidth">
+				
+				
+				<article class="post">
+					<header class="post-title">
+						<h1>El título de una página</h1>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</article>	<!-- article -->
+				
+				
+			</section><!-- /#main-content -->
+			
+		</section><!-- /#global-content -->
+		
+		
+		
+		<footer id="main-footer">
+			
+			<div class="footer-copyright">
+				&copy; 2014 Tema de prueba
+			</div><!-- /.footer-copyright -->
+			
+		</footer><!-- footer -->
+	
+	</div><!-- /#global-container -->
+
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?> 
+			<section id="main-content">
+				
+				<article class="post resume">
+					<header class="post-title">
+						<h2><a href="">El título de un post</a></h2>
+						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+					</div>
+				</article>	<!-- article -->
+				
+				
+				<article class="post resume">
+					<header class="post-title">
+						<h2><a href="">El título de un post</a></h2>
+						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+					</div>
+				</article>	<!-- article -->
+				
+				
+				<article class="post resume">
+					<header class="post-title">
+						<h2><a href="">El título de un post</a></h2>
+						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+					</div>
+				</article>	<!-- article -->
+				
+				
+				<article class="post resume">
+					<header class="post-title">
+						<h2><a href="">El título de un post</a></h2>
+						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+					</div>
+				</article>	<!-- article -->
+				
+				
+				<article class="post resume">
+					<header class="post-title">
+						<h2><a href="">El título de un post</a></h2>
+						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+					</header>
+					<div class="post-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+					</div>
+				</article>	<!-- article -->
+				
+				<div class="posts-nav cf">
+					<a href="" >&larr; Previos</a>
+					<a href="" >Recientes &rarr;</a>
+				</div>
+				
+				
+			</section><!-- /#main-content -->
+			
+			
+			<aside id="sidebar">
+				
+				<div class="widget">
+					<h3 class="widget-title">Widget de texto</h3>
+					<div class="widget-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+				
+				
+				
+			</aside><!-- /#sidebar -->
+			
+		</section><!-- /#global-content -->
+		
+		
+		<footer id="main-footer">
+			
+			<div class="footer-copyright">
+				&copy; 2014 Tema de prueba
+			</div><!-- /.footer-copyright -->
+			
+		</footer><!-- footer -->
+	
+	</div><!-- /#global-container -->
+
+	
+</body>
+</html>
